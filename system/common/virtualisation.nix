8acbbfa85407a62f25998ff3b75a75108904bbc7
DylanSoule <dylan.soule@icloud.com>
2026-09-20 16:51:17 -0600

    Remove vbox as don't use/need it

--- system/common/virtualisation.nix
+++ system/common/virtualisation.nix
@@ -1,16 +1,5 @@
 { pkgs, ... }:
 
 {
-  virtualisation.virtualbox.host = {
-    enable = true;
-  };
-  users.extraGroups.vboxusers.members = [ "dylans" ];
-  xdg.portal = {
-    enable = true;
-    extraPortals = with pkgs; [
-      xdg-desktop-portal-gtk
-      # xdg-desktop-portal-wlr # Optional: include if using wlroots compositors like Sway/River
-    ];
-  };
   virtualisation.vmware.host.enable = true;
 }
